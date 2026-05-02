a8abd34a32692e0581d3dc271727ee91fe770568
Arnab Pachal <arnabpachal2004@gmail.com>
2026-05-02 21:31:47 +0530

    This is the solution of 02 may 2026 Question : The position of set bit ( error solved)

--- 02 May 2026/Solution.java
+++ 02 May 2026/Solution.java
@@ -1,7 +1,7 @@
 class Solution {
     public int findPosition(int n) {
         // code here
-        if(n&(n-1)!=0) return -1;
+        if((n&(n-1))!=0) return -1;
         
         int cnt =1;
         while(n>0)
