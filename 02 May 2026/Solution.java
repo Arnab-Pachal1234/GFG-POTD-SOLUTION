778f772c4520dec6744f951fa1c06e9e7db6a317
Arnab Pachal <arnabpachal2004@gmail.com>
2026-05-02 21:31:07 +0530

    This is the solution of 02 may 2026 Question : The position of set bit

---
+++ 02 May 2026/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findPosition(int n) {
+        // code here
+        if(n&(n-1)!=0) return -1;
+        
+        int cnt =1;
+        while(n>0)
+        {
+         if((n&1)==1) return cnt;
+         n>>=1;
+         cnt++;
+        }
+        return cnt ;
+        
+    }
+}
